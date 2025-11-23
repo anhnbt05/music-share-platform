10fb64de4ac98ff3f5d5f275f551fec36882daa5
Nguyen Ba Tuan Anh <nguyenbatuananh2k5@gmail.com>
2025-11-23 17:08:59 +0700

    feat: build base spring boot for project

---
+++ src/main/java/com/example/hihi/MusicShareServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hihi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicShareServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicShareServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hihi/HihiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hihi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HihiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
